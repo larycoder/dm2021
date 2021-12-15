9dab09fb0fc1643becdb06c3efbf5cf4326bbaef
larycoder <lenhuchuhiep99@gmail.com>
2021-12-15 16:08:22 +0700

    LH    : practice 04 - kmean algorithm with 50 sample

---
+++ 04.review.length.kmeans.py
@@ -0,0 +1,133 @@
+from random import random
+from typing import List
+
+import lib
+from lib import BaseCluster, Point
+
+
+# inspect tool
+def print_cluster(clusters: list):
+    # display result
+    print("Kmean result:\n")
+    for cluster in clusters:
+        cluster.display()
+        print("\n")
+
+
+# load data
+docs = lib.load_docs_from_file(
+    "./yelp_academic_500-head-sample.json",
+    "text",
+    count=50, # number of sample with maximum is 500
+)
+data = [len(doc) for doc in docs]
+
+
+# implement kmeans
+class KmeanCluster(BaseCluster):
+    def __init__(self, value_len: int, centroid: Point = None):
+        '''
+        initialize a cluster object supporting kmean algorithm
+
+        Parameters
+        ----------
+        value_len: int
+            maximum length of point value in Point list
+            which is used to decide random initial centroid
+            if centroid is not assigned
+        centroid: Point
+            centroid point of cluster. If centroid is not
+            passed, a random one will be assigned
+        '''
+        super().__init__()
+        self.centroid = Point(0)
+        self.old_centroid = Point(0)
+        if centroid:
+            self.centroid = centroid
+        else:
+            self.centroid = Point(int(random() * value_len))
+
+    def dist(self, point: Point):
+        '''
+        compute Euclidean distance from point to cluster centroid
+        '''
+        return self.centroid.dist(point)
+
+    def update_centroid(self):
+        '''
+        update to new centroid which is mean of all elements
+        '''
+        total = Point(0)
+        for i in self.els:
+            total += i
+        self.old_centroid.v = self.centroid.v
+        if len(self.els) > 0:
+            self.centroid.v = int(total.v / len(self.els))
+
+    def is_moved(self):
+        '''
+        comparing current centroid position to its old position
+        '''
+        return not (self.centroid.v == self.old_centroid.v)
+
+    def display(self):
+        print("---------")
+        print("Kmean cluster info:")
+        print("Centroid: ", self.centroid)
+        print("Elements list:")
+        print(self.els)
+        print("---------")
+
+
+class Kmean:
+    def __init__(self, clusters: List[KmeanCluster], data: List[Point]):
+        self.clusters = clusters
+        self.data = data
+
+    def update_cluster(self):
+        '''
+        add points to cluster
+        '''
+        for point in self.data:
+            dist = []
+            for cluster in self.clusters:
+                dist.append({
+                    "cluster": cluster,
+                    "distance": cluster.dist(point)
+                })
+            cluster = min(dist, key=lambda ip: ip["distance"])["cluster"]
+            cluster.add(point)
+
+    def update_cluster_centroid(self):
+        '''
+        re-update clusters following point data
+        '''
+        # update point in each cluster
+        self.update_cluster()
+
+        # update cluster centroid by its point
+        for cluster in self.clusters:
+            cluster.update_centroid()
+
+        # clear point in cluster
+        for cluster in self.clusters:
+            cluster.clear()
+
+    def run(self):
+        '''
+        loop update centroid until nothing move
+        '''
+        while True:
+            self.update_cluster_centroid()
+            if all(not cluster.is_moved() for cluster in self.clusters):
+                break
+
+
+if __name__ == "__main__":
+    p_data = [Point(d) for d in data]
+    p_max_len = max(data)
+    clusters = [KmeanCluster(p_max_len) for i in range(3)]
+    kmean = Kmean(clusters, p_data)
+    kmean.run()
+    kmean.update_cluster()
+    print_cluster(clusters)
--- lib.py
+++ lib.py
@@ -1,7 +1,7 @@
 import json
+from typing import List
 
-
-def load_docs_from_file(filename, key, count = None):
+def load_docs_from_file(filename, key, count=None):
     documents = []
     with open(filename, "r") as f:
         idx = 0
@@ -13,3 +13,45 @@ def load_docs_from_file(filename, key, count = None):
                 break
             idx += 1
     return documents
+
+
+# Graph class
+class Point:
+    def __init__(self, values):
+        self.v = values
+
+    def dist(self, point) -> int:
+        '''
+        compute Euclidean distance to another Point
+        '''
+        return abs(point.v - self.v)
+
+    def __add__(self, other):
+        '''
+        add my value with other Point value
+        '''
+        return Point(self.v + other.v)
+
+    def __str__(self):
+        return f"Point(v={self.v})"
+
+    def __repr__(self):
+        return self.__str__()
+
+
+class BaseCluster:
+    def __init__(self, data: list = []):
+        self.els = []
+        self.els += data
+
+    def add(self, el):
+        '''
+        add new point into cluster
+        '''
+        self.els.append(el)
+
+    def clear(self):
+        '''
+        clear all element in cluster
+        '''
+        self.els.clear()
